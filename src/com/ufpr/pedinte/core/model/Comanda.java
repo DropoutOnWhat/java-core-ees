66371b5ab2a43ce6805dcaa761e566549fb8592c
Reggie <regisgaboardi@gmail.com>
2020-09-14 09:43:54 -0300

    Adicionado apache lang e mysql connector. Padronizado nomes dos pacotes e aualizadas referencias. Gerado novo JAR.

---
+++ src/com/ufpr/pedinte/core/model/Comanda.java
@@ -0,0 +1,22 @@
+package com.ufpr.pedinte.core.model;
+
+public class Comanda {
+    private String produto;
+    private int quantia;
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantia() {
+        return quantia;
+    }
+
+    public void setQuantia(int quantia) {
+        this.quantia = quantia;
+    }
+}
---
+++ src/com/ufpr/pedinte/core/model/ItemDoPedido.java
@@ -0,0 +1,32 @@
+package com.ufpr.pedinte.core.model;
+
+public class ItemDoPedido {
+
+    int quantidade;
+    Produto produto;
+
+    public ItemDoPedido() {
+
+    }
+
+    public ItemDoPedido(int quantidade, Produto produto) {
+        this.quantidade = quantidade;
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
